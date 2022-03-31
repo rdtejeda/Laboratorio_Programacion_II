d264580fa9604bdca92158609a72c9abb2f9bd6a
rdtejeda <roberto.daniel.tejeda@gmail.com>
2022-03-31 13:26:08 -0300

    31marzoII

--- ClasesMetodosEstatico/I01ValidadorRango/Program.cs
+++ ClasesMetodosEstatico/I01ValidadorRango/Program.cs
@@ -1,6 +1,17 @@
 ﻿using System;
 using Biblioteca;
-
+/*
+Consigna#
+Realizar una clase llamada Validador que posea un método estático llamado Validar con la siguiente firma:
+bool Validar(int valor, int min, int max)
+valor: dato a validar.
+min: mínimo valor incluido.
+max: máximo valor incluido.
+Pedir al usuario que ingrese 10 números enteros. Validar con el método desarrollado anteriormente que estén dentro del rango -100 y 100.
+Terminado el ingreso mostrar el valor mínimo ingresado, valor máximo ingresado y el promedio.
+IMPORTANTE
+Utilizar variables escalares, NO utilizar vectores/arrays.
+*/
 namespace I01ValidadorRango
 {
     internal class Program
